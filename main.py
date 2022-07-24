9aa139768ab51af158fe64c69fa1eb51ff3dbf67
Emily Pham <emilyn3@hawaii.edu>
2022-07-23 14:30:02 -1000

    .env variables and inital command
    Stored token as an .env variable. Implemented starting command in both English and Japanese.

--- main.py
+++ main.py
@@ -4,4 +4,28 @@
 #
 # Author: Emily Pham <emilyn3@hawaii.edu>
 
+from dotenv import load_dotenv
+import os
 import discord
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('~play'):
+        await message.channel.send("Let's Play!")
+    if message.content.startswith('～遊びましょう'):
+        await message.channel.send("いっしょに遊ぼうよ～")
+
+client.run(TOKEN)
+
